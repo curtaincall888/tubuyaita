a7028729935aff23d1a8a5c5ab7362454baaa060
Kota Tsutsumi <kota3730@gmail.com>
2020-07-28 20:46:05 +0900

    add_sign_in
    ログイン機能実装

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,38 @@
       <h1>tubuyaita</h1>
     </router-link>
     <div class="btns">
-      <button>
+      <button @click="signIn">
         <fa icon="user" />
       </button>
     </div>
   </header>
 </template>
+
+<script>
+import firebase from 'firebase'
+import { auth } from '../main'
+export default {
+  methods: {
+    signIn() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      auth.signInWithPopup(provider)
+      .then((result) => {
+        alert('Hello, '+result.user.displayName+'!')
+      })
+    },
+    signOut() {
+      if (window.confirm('Are you Sure to Sign Out ?')) {
+        auth.signOut()
+        .then(() => {
+          alert('You Safely Signed Out.')
+          this.$router.push('/'),
+          location.reload()
+        })
+      }
+    }
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 header
