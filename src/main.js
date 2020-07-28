196bfb725a032488d9e33ae90f5f3c2376ee8daa
Kota Tsutsumi <kota3730@gmail.com>
2020-07-28 15:51:06 +0900

    conection_firebase
    firebaseとアプリの連携

--- src/main.js
+++ src/main.js
@@ -14,7 +14,24 @@ library.add(
   faSignOutAlt,
   faEllipsisV
 )
+import { firestorePlugin } from "vuefire";
+import firebase from "firebase";
+import "firebase/firestore";
 
+Vue.use(firestorePlugin);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBG8biSkvrFD3IdGhskjMlU_SDVQLBRGQs",
+  authDomain: "tubuyaita-55e28.firebaseapp.com",
+  databaseURL: "https://tubuyaita-55e28.firebaseio.com",
+  projectId: "tubuyaita-55e28",
+  storageBucket: "tubuyaita-55e28.appspot.com",
+  messagingSenderId: "151449870416",
+  appId: "1:151449870416:web:1dbbddf39e187b39177155",
+});
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
 Vue.component('fa', FontAwesomeIcon)
 Vue.config.productionTip = false
 
